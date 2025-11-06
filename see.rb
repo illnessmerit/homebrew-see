663c1a16b3da3bf0659168072189f2b922f70bd4
a <a@as-Virtual-Machine.local>
2025-11-05 19:39:20 -0800

    Enable RuboCop pre-commit hook with autocorrect configuration

--- see.rb
+++ see.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# This class represents a Homebrew formula for the see project.
 class See < Formula
   url 'https://github.com/8ta4/see/releases/download/v0.1.0/bin.zip'
   sha256 'f5669741121beee1f59a53e947022175dcaf67dece495bf02408fe9a15f615ef'
