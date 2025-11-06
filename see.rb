fbeedd3a30a6a8b9e233713a5cf5d13b9ec84a54
a <a@as-Virtual-Machine.local>
2025-11-05 19:52:46 -0800

    Reorder binary installation to install host before see

--- see.rb
+++ see.rb
@@ -5,7 +5,7 @@ class See < Formula
   url 'https://github.com/8ta4/see/releases/download/v0.1.0/bin.zip'
   sha256 'f5669741121beee1f59a53e947022175dcaf67dece495bf02408fe9a15f615ef'
   def install
-    bin.install 'see'
     bin.install 'host'
+    bin.install 'see'
   end
 end
