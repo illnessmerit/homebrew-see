4ec4e80809ded1369cc95171c29e5e1f390c460f
a <a@as-Virtual-Machine.local>
2025-11-08 22:03:12 -0800

    Switch archive format from zip to tar.gz in formula

--- see.rb
+++ see.rb
@@ -2,8 +2,8 @@
 
 # This class represents a Homebrew formula for the see project.
 class See < Formula
-  url 'https://github.com/8ta4/see/releases/download/v0.1.0/bin.zip'
-  sha256 'f5669741121beee1f59a53e947022175dcaf67dece495bf02408fe9a15f615ef'
+  url 'https://github.com/8ta4/see/releases/download/v0.1.1/bin.tar.gz'
+  sha256 'da7f592b98e54826c4ee622d60f9371ca60cecc06b0b45cf19004b1f0f10d8e5'
   def install
     bin.install 'host'
     bin.install 'see'
